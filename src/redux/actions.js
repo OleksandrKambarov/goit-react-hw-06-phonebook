f8b678b4afac68d374ea3b80f57a94626db8c77a
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-05-14 23:22:30 +0300

    +

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,14 @@
-import { createAction } from "@reduxjs/toolkit";
-const shortid = require("shortid");
+import { createAction } from '@reduxjs/toolkit';
 
-export const addContact = createAction("contacts/add", (data) => ({
+import { nanoid } from 'nanoid';
+
+export const addContact = createAction('contacts/add', data => ({
   payload: {
     ...data,
-    id: shortid.generate(),
+    id: nanoid(),
   },
 }));
 
-export const deleteContact = createAction("contacts/delete");
+export const deleteContact = createAction('contacts/delete');
 
-export const filterContact = createAction("contacts/changeFilter");
+export const filterContact = createAction('contacts/changeFilter');
